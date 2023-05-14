7cd720109516778052c9cf5007c00991053e422c
shashwat0756 <shashwatsandilya514@gmail.com>
2023-05-14 22:48:28 +0530

    Implementing edit button

--- gui.py
+++ gui.py
@@ -2,8 +2,20 @@ import function
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip = "Enter todo")
+input_box = sg.InputText(tooltip = "Enter todo",key = "todo")
 add_button = sg.Button("Add")
-window = sg.Window("My To-do App",layout=[[label],[input_box]])
-window.read()
+window = sg.Window("My To-do App",layout=[[label],[input_box],[add_button]],font =("vintage",20))
+while True:
+   event,values = window.read()
+   print(event)
+   print(values)
+   match event:
+       case "Add":
+           todos = function.get_todos()
+           new_todo = values["todo"] + "\n"
+           todos.append(new_todo)
+           function.write_todos(todos)
+       case sg.WIN_CLOSED:
+           break
+
 window.close()
